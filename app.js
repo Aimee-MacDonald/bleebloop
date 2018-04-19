d14cc0e4e56b496c6bcc40ffe2d99354c96d81e3
Aimée <admin@aimeemacdonald.com>
2018-04-19 12:29:09 +0100

    dotenv final

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ app.set("view engine", "pug");
 app.use(express.static(__dirname + "/static"));
 
 app.get("/", (req, res) => {
-  console.log("Environment Code: " + process.env.CODE);
   res.render("index");
 });
 
-app.listen(process.env.PORT || 8080, console.log(process.env.PORT || 8080));
+app.listen(process.env.PORT || 8080);
