363f6735f70fa22ef88172516aace788ce01d085
Aimée <admin@aimeemacdonald.com>
2018-04-19 13:27:25 +0100

    Email Capturing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const Email = require(__dirname + "/dbmodels/email");
@@ -11,6 +12,22 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
 
 app.use(express.static(__dirname + "/static"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post("/subscribe", (req, res) => {
+  let newEmail = new Email({
+    email: req.body.email
+  });
+
+  newEmail.save(err => {
+    if(err) throw err;
+  });
+  
+  res.render("thanks");
+});
 
 app.get("/", (req, res) => {
   res.render("index");
